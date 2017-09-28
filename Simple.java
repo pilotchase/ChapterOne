bceb9ca946c17b8e1a9c73f34450b64a7db47c2b
Chase Morgan <pilotchase@users.noreply.github.com>
2017-09-28 12:07:11 -0700

    Simple

---
+++ Simple.java
@@ -0,0 +1,23 @@
+// ********************************************
+//  Simple.java
+//
+//  Print a simple message about Java.
+//  	    
+// ********************************************
+ 
+//public class simple // works
+//public class SimpleProgram // works
+//public class 1SimpleProgram // doesn't work
+//public class _Simple_ //doesn't work
+//public class *Simple* // doesn't work
+//public class $123_45 // doesn't work
+//public class Simple! // doesn't work
+{
+ 
+	public static void main (String[] args)
+	{
+ 
+  	System.out.println ("Java rocks!!");
+ 
+	}
+}
