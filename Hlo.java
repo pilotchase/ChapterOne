8cbbd39b905f27ffcd5306cc6e6831315372df06
Chase Morgan <pilotchase@users.noreply.github.com>
2017-09-28 12:11:31 -0700

    Hello

---
+++ Hlo.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Hello here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+// #1 No error
+// #2 No error because you can say whatever you want
+// #3 Unclosed string literal
+// #4 ')' expected
+// #5 ';' expected
+public class Hlo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!")
+    }
+}
