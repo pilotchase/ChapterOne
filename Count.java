f7833673b325d3db6e5fe49797c73e4b88b7a99a
Chase Morgan <pilotchase@users.noreply.github.com>
2017-09-28 12:04:05 -0700

    Count

---
+++ Count.java
@@ -0,0 +1,21 @@
+
+/**
+ * counts from 1 to 5 in English, French, and Spanish
+ *
+ * @author C. Morgan
+ * @version 09/28/17
+ */
+public class Count
+{
+    public static void main (String[] args)
+    {
+    // English
+    System.out.println ("one two three four five");
+    // French
+    System.out.println ("un deux trois quatre cinq");
+    // Spanish // No this comment is treated just the same
+    System.out.println ("uno dos tres cuatro cinco");
+ 
+    }
+
+}
